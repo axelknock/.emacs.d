a67ac8f0d2ccf98da27bc07a42b61203f74af584
Lucian Knock <me@lucianknock.com>
2024-03-26 08:40:00 -0400

    Add star character to replacement + tests

--- homegrown/handleize.el
+++ homegrown/handleize.el
@@ -8,7 +8,7 @@
 (defun my/handleize-string (s)
   "Handleize S."
   (downcase (s-replace-regexp "[ ’]" "-"
-			      (s-replace-regexp "[!?.'`,\"()<>;:]" "" s))))
+			      (s-replace-regexp "[!?.'`,\"()<>;:\*]" "" s))))
 
 (defun my/handleize-region ()
   "Handleize currently selected region."
@@ -22,5 +22,18 @@
   (interactive)
   (while (re-search-forward ">\(.+\)</h2" nil t)
     (replace-match (match-string 1) nil nil)))
+
+(defun my/test-handleize-string ()
+  "Test cases for `my/handleize-string'."
+  (should (equal (my/handleize-string "This is a test string") "this-is-a-test-string"))
+  (should (equal (my/handleize-string "Hello, World!") "hello-world"))
+  (should (equal (my/handleize-string "Some Pu*nct?uation!") "some-punctuation"))
+  (should (equal (my/handleize-string "rándom'Cäse!") "random-case"))
+  (should (equal (my/handleize-string "  Multiple   Spaces  ") "multiple-spaces"))
+  (should (equal (my/handleize-string "") "")))
+
+;;; handleize.el ends here
+
+(provide 'handleize)
 
 ;;; handleize.el ends here
